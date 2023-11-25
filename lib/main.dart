66fbe111f968cd861836e5cb7db238302c96be48
Cimorexave <sadeq.s502@gmail.com>
2023-11-25 17:09:49 +0330

    text theme

--- lib/main.dart
+++ lib/main.dart
@@ -12,24 +12,32 @@ class App extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData.from(
           colorScheme: const ColorScheme(
-        primary: Colors.blue, // A darker variant of the primary color
-        secondary: Colors.orange, // A darker variant of the secondary color
-        surface: Colors
-            .white, // The background color for surfaces such as cards and dialogs
-        background: Colors.white, // The background color for your app
-        error: Colors.red, // The error color for your app
-        onPrimary: Colors
-            .white, // The color to use for text on top of the primary color
-        onSecondary: Colors
-            .black, // The color to use for text on top of the secondary color
-        onSurface: Colors.black, // The color to use for text on top of surfaces
-        onBackground: Colors
-            .black, // The color to use for text on top of the background color
-        onError:
-            Colors.white, // The color to use for text on top of the error color
-        brightness: Brightness
-            .light, // The overall brightness of the theme (light or dark)
-      )),
+            primary: Colors.blue, // A darker variant of the primary color
+            secondary: Colors.orange, // A darker variant of the secondary color
+            surface: Colors
+                .white, // The background color for surfaces such as cards and dialogs
+            background: Colors.white, // The background color for your app
+            error: Colors.red, // The error color for your app
+            onPrimary: Colors
+                .white, // The color to use for text on top of the primary color
+            onSecondary: Colors
+                .black, // The color to use for text on top of the secondary color
+            onSurface:
+                Colors.black, // The color to use for text on top of surfaces
+            onBackground: Colors
+                .black, // The color to use for text on top of the background color
+            onError: Colors
+                .white, // The color to use for text on top of the error color
+            brightness: Brightness
+                .light, // The overall brightness of the theme (light or dark)
+          ),
+          textTheme: const TextTheme(
+            headline1: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            headline2: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            headline3: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            bodyText1: TextStyle(fontSize: 16),
+            bodyText2: TextStyle(fontSize: 14),
+          )),
       home: Scaffold(
         floatingActionButton: FloatingActionButton(
           backgroundColor: Theme.of(context).primaryColor,
